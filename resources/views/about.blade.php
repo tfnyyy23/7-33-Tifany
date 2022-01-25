19dffacad848fff07afe24f4c5618dcfa71cd8f6
tfnyyy23 <fadillahtifany@gmail.com>
2022-01-25 20:14:02 +0700

    ubah isi about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Tifany | About</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-    <h3> <?= $nama; ?> </h3>
-    <h3>Amin......</h3>
-    <p> <?= $email; ?> </p>
-    <img src="images/<?= $gambar; ?>" alt="<?= $nama; ?>" width="200px">
+@extends('layouts.main')
 
-</body>
-</html>
+@section('container')
+    <h1>Halaman About</h1>
+    <h3> {{ $nama }} </h3>
+    <p> {{ $email }} </p>
+    <img src="images/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+@endsection
