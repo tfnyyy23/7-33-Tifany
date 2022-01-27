1d7724490efddfb97284edbf69dce01458e49343
tfnyyy23 <fadillahtifany@gmail.com>
2022-01-27 10:16:24 +0700

    endpoint '/contacts'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -49,3 +50,5 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+Route::resource('/contacts', ContactController::class); 
